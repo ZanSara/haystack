336c28d8f47632e6050ce335904dc19cda2b9e44
Mateusz Haligowski <mhaligowski@gmail.com>
2023-10-26 14:05:29 -0700

    Gradient embeddings (#2)

--- haystack/preview/components/generators/__init__.py
+++ haystack/preview/components/generators/__init__.py
@@ -1,5 +1,6 @@
+from haystack.preview.components.generators.gradient.base import GradientGenerator
 from haystack.preview.components.generators.hugging_face_local import HuggingFaceLocalGenerator
 from haystack.preview.components.generators.hugging_face_tgi import HuggingFaceTGIGenerator
 from haystack.preview.components.generators.openai import GPTGenerator
 
-__all__ = ["HuggingFaceLocalGenerator", "HuggingFaceTGIGenerator", "GPTGenerator"]
+__all__ = ["GPTGenerator", "GradientGenerator", "HuggingFaceTGIGenerator", "HuggingFaceLocalGenerator"]
